5a456f88353b45807bd6c984b371cd376b3b375a
Herasil <78405491+Herasil@users.noreply.github.com>
2022-08-16 02:44:13 +0300

    New ArrayControls class with constructor and array grouping

--- uzduotis_2.php
+++ uzduotis_2.php
@@ -1,4 +1,21 @@
 <?php
-$data = [1, 2, 4, 7, 1, 6, 2, 8];
 
+class ArrayControls
+{
+    function __construct($array){
+        $this->array = $array;
+    }
+
+    function newArrays($groups){
+        $arrays = array();
+        for ($array=0;$array<$groups;$array++){
+            array_push($arrays, array());
+        }
+        return $this->arrays = $arrays;
+    }
+}
+
+
+$data = [1, 2, 4, 7, 1, 6, 2, 8];
+$groups = 3;
 ?>
