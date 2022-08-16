9d12152040f86b5ff43e0f26c6585424bd0883de
Herasil <78405491+Herasil@users.noreply.github.com>
2022-08-16 03:03:01 +0300

    Method for finding smallest arrays ID

--- uzduotis_2.php
+++ uzduotis_2.php
@@ -6,16 +6,28 @@ class ArrayControls
         $this->array = $array;
     }
 
-    function newArrays($groups){
+    function newArrays($groupsCount): array{
         $arrays = array();
-        for ($array=0;$array<$groups;$array++){
+        for ($array=0;$array<$groupsCount;$array++){
             array_push($arrays, array());
         }
         return $this->arrays = $arrays;
     }
+
+    function findSmallestArraysElementID(): int{
+        $arrayElementID = 0;
+        $smallestSum = array_sum($this->arrays[$arrayElementID]);
+        for ($i = 1; $i < count($this->arrays); $i++){
+            $currentSum = array_sum($this->arrays[$i]);
+            if($currentSum < $smallestSum) {
+                $smallestSum = $currentSum;
+                $arrayElementID = $i;
+            }
+        }
+        return $arrayElementID;
+    }
 }
 
-
 $data = [1, 2, 4, 7, 1, 6, 2, 8];
-$groups = 3;
+$groupsCount = 3;
 ?>
