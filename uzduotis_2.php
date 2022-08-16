7d32d3487fee3ca8942d39d6df2d476d82e43f13
Herasil <78405491+Herasil@users.noreply.github.com>
2022-08-16 03:12:41 +0300

    Class methods to getarraygroups and print results

--- uzduotis_2.php
+++ uzduotis_2.php
@@ -26,8 +26,28 @@ class ArrayControls
         }
         return $arrayElementID;
     }
+
+    function getAllArrayGroups($groupsCount){
+        $this->newArrays($groupsCount);
+        for($i= 0;$i < count($this->arrays); $i++){
+            array_push($this->arrays[$this->findSmallestArraysElementID()],$this->arrays[$i]);
+        }
+        return $this->arrays;
+    }
+
+    function printArrays(){
+        foreach($this->arrays as $item){
+            print_r($item);
+            print_r(" Gauta suma: ");
+            echo array_sum($item);
+        }
+    }
 }
 
 $data = [1, 2, 4, 7, 1, 6, 2, 8];
 $groupsCount = 3;
+$classObject = new ArrayControls($data);
+$classObject->getAllArrayGroups($groupsCount);
+$classObject->printArrays();
+
 ?>
